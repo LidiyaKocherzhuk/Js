4e0b99e195033a8150a2ce134cf3482b8fe89a1f
LidiyaKocherzhuk <lidiyaonufreyiv31@gmail.com>
2021-11-26 14:32:17 +0200

    lesson5 additional

--- lesson5/additional5.js
+++ lesson5/additional5.js
@@ -30,66 +30,51 @@ fnNum(95, 65);
 //   foo([9,8,0,4], 0) // ==> [ 8, 9, 0, 4 ]
 //   foo([9,8,0,4], 1) // ==> [ 9 ,0, 8, 4 ]
 //   foo([9,8,0,4], 2) // ==> [ 9, 8, 4, 0 ]
-// let foo = [9,8,0,4];
-//
-// let fooIndex = (arr, i) => {
-//     let index = arr[i];
-//     arr[i] = arr[i + 1];
-//     arr[i + 1] = index;
-//     console.log(arr)
-// };
-// fooIndex(foo, 0);
+let foo = [9,8,0,4];
+
+let fooIndex = (arr, i) => {
+    let index = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i + 1] = index;
+    console.log(arr)
+};
+fooIndex(foo, 0);
 
 // - Сворити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
 // Двожина масиву від 2 до 100
 // EXAMPLE:
-// [1,0,6,0,3] => [1,6,3,0,0]
-// [0,1,2,3,4] => [1,2,3,4,0]
-// [0,0,1,0]   => [1,0,0,0]
-let array = [2,0,8,6,45,0,0,9,5];
-console.log(array)
-// let b=array.length;
-// let v=b
-// for(let i=0;i<b;i++){
-//     if (array[i]===0){
-//                 array[v++]=array[i]
-//         console.log(i)
-//         console.log(b)
-// for (let c=i;c<b;c++){
-//     let q=c
-//
-//     array[q]=array[++q]
-//
-//     console.log(array)
-//
-// }
-//         array.length--
-//
-//
-//              }
-//
-// }
-// document.write(array);
-//
+let arr1 = [1, 0, 6, 0, 3] //=> [1,6,3,0,0]
+let arr2 = [0, 1, 2, 3, 4] //=> [1,2,3,4,0]
+let arr3 = [0, 0, 1, 0]   //=> [1,0,0,0]
 
+let array = [2, 0, 8, 6, 45, 0, 5, 0, 9, 5];
 
-
-
-
-
-
-
-
-
-// array[b]=i;
-
-let fnZeroEnd = (arr) => {
-    let splice = arr[0];
-    for (let i = 0;arr.length; i++) {
+function fnZeroEnd(arr) {
+    let newArr = '', j = 0;
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] === 0) {
-            splice = arr.splice(i,i);
+            newArr += arr.splice(i, 1);
+
+            if (arr[i] === 0) {
+                fnZeroEnd(arr);
+            }
+            arr.push(Number(newArr[j]));
+        }
     }
+    return arr;
 }
-        console.log(arr)
-};
-fnZeroEnd(array)
+
+console.log(fnZeroEnd(array));
+console.log(fnZeroEnd(arr1));
+console.log(fnZeroEnd(arr2));
+console.log(fnZeroEnd(arr3));
+
+
+
+
+
+
+
+
+
+
