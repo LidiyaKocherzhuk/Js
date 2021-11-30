741b531237b099ed42a10fb3660bcd0b8e36cdb2
LidiyaKocherzhuk <lidiyaonufreyiv31@gmail.com>
2021-11-30 23:18:09 +0200

    lesson7 additional

---
+++ lesson7/additional/сlosures7.js
@@ -0,0 +1,48 @@
+function userCard(number) {
+    let cardOption = new getCardOptions(number);
+    console.log(cardOption);
+    putCredits(150, cardOption);
+    takeCredits(100, cardOption);
+    setTransactionLimit(5000, cardOption);
+    let card1 = new getCardOptions(1);
+
+}
+    function getCardOptions(key){
+            this.balance = 100;
+            this.transactionLimit = 100;
+            this.historyLogs = [
+                {operationType: '', credits: 474, operationTime: ""},
+                {operationType: '', credits:47 , operationTime: ""},
+                {operationType: '', credits:7, operationTime: ""},
+            ];
+            this.key = key;
+    }
+
+function putCredits(putNum, user) {
+    user.balance = user.balance + putNum;
+}
+
+function takeCredits(takeNum, user) {
+    if (user.balance > takeNum && user.transactionLimit > takeNum) {
+    user.balance = user.balance - takeNum;
+    }
+    else {
+       console.error('Не вистачає коштів або або ліміт є меншим!')
+    }
+}
+
+function setTransactionLimit(transactionNum, user) {
+    user.transactionLimit = transactionNum;
+}
+
+function transferCredits(transferNum, user, recipient) {
+    if (user.balance > transferNum && user.transactionLimit > transferNum) {
+        user.balance = user.balance - transferNum;
+
+    }
+    recipient.balance = recipient.balance + transferNum;
+}
+
+
+userCard(3);
+// let card1 = new userCard(1);
