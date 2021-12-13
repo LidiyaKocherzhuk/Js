911dc8cab20e2820e3f238833249082f8aae660d
LidiyaKocherzhuk <lidiyaonufreyiv31@gmail.com>
2021-12-13 12:03:27 +0200

    lesson12

--- lesson12/classwork12.js
+++ lesson12/classwork12.js
@@ -3,11 +3,15 @@
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
 
+let divElement = document.createElement('div');
+divElement.classList.add('postsContent');
+
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(responsePosts => {
         responsePosts.forEach(post => {
-
+            let divPostComment = document.createElement("div");
+            divPostComment.classList.add('divPostComment');
             let ulPost = document.createElement("ul");
             let buttonComment = document.createElement('button');
             ulPost.classList.add('ul_post');
@@ -17,7 +21,7 @@ fetch('https://jsonplaceholder.typicode.com/posts')
                 let li = document.createElement('li');
                 let h3 = document.createElement('h3');
                 let h4 = document.createElement('h4');
-                li.classList.add('li_flex');
+                li.classList.add('d_flex');
                 h3.classList.add('w_h3');
                 h4.classList.add('w_h4')
                 h3.innerHTML = `${key}:`
@@ -25,40 +29,48 @@ fetch('https://jsonplaceholder.typicode.com/posts')
                 li.append(h3, h4);
                 ulPost.appendChild(li);
             }
-            let divBlocComments = document.createElement('div');
-            divBlocComments.id = 'divBlocComments'
-
-
-            fetch('https://jsonplaceholder.typicode.com/comments')
-                .then(response => response.json())
-                .then(responseComments => {
-
             buttonComment.onclick = function () {
-                        divBlocComments.classList.toggle('show_comment')
-                        let show_commentElement = document.getElementsByClassName('show_comment')[0];
+                let divComments = document.createElement('div');
+                divComments.classList.add('comments');
+                getComment(post.id, divComments);
+                divPostComment.appendChild(divComments);
+                buttonComment.disabled = true;
+            };
 
-                responseComments.forEach(comment => {
-                            if (comment.id === post.id) {
-                                for (const key in comment) {
-                                    let h3 = document.createElement('h3');
-                                    let h4 = document.createElement('h4');
-                                    h3.classList.add('w_h3');
-                                    h4.classList.add('w_h4')
-                                    h3.innerHTML = `${key}:`
-                                    h4.innerHTML = `${comment[key]}:`
-                                    divBlocComments.append(h3, h4);
-                                }
-                            }
-
-                        });
-
-                show_commentElement.innerHTML = divBlocComments;
-                    }
-                    document.body.appendChild(divBlocComments);
-                })
 
             ulPost.appendChild(buttonComment);
-            document.body.appendChild(ulPost);
+            divPostComment.appendChild(ulPost);
+            divElement.appendChild(divPostComment);
+            document.body.appendChild(divElement);
 
         });
     });
+
+
+function getComment(id, append) {
+fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(response => response.json())
+    .then(responseComments => {
+        responseComments.forEach(comment => {
+            console.log(comment)
+            if (comment.postId === id) {
+    let blocComments = document.createElement('div');
+            blocComments.classList.add('show');
+                for (const key in comment) {
+                    let h4 = document.createElement('h4');
+                    h4.classList.add('w_h4')
+                    h4.innerHTML = `<u>${key}:</u>   <i>${comment[key]}</i>`
+                    blocComments.append( h4);
+                }
+           append.appendChild(blocComments);
+            }
+
+        })
+    })
+}
+
+
+
+
+
+
