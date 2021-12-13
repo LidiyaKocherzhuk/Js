4404e475fb6da4e2266f606bba063551b602eafe
LidiyaKocherzhuk <lidiyaonufreyiv31@gmail.com>
2021-12-13 17:25:08 +0200

    lesson12

--- lesson12/additional/additional12.js
+++ lesson12/additional/additional12.js
@@ -3,6 +3,7 @@
 // https://jsonplaceholder.typicode.com/users
 //     кожному елементу юзера створити кнопку, при клику на яку в окремий блок виводяться всі пости поточного юзера.
 //     Кожному елементу post створити кнопку, при клику на яку в окремий блок виводяться всі коментарі поточного поста
+
 function getComment(placeholder,id, append) {
     fetch(placeholder)
         .then(response => response.json())
@@ -23,3 +24,36 @@ function getComment(placeholder,id, append) {
             })
         })
 }
+            let divElement = document.createElement('div');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(responseUsers => {
+        responseUsers.forEach(user =>{
+            let divUserCommentPost = document.createElement('div');
+            let divUser = document.createElement('div');
+
+            for (const keyUser in user) {
+                let h3 = document.createElement('h3');
+                h3.innerHTML = `${keyUser} - ${user[keyUser]}`
+                    divUser.appendChild(h3);
+            }
+
+            divUserCommentPost.appendChild(divUser);
+            divElement.appendChild(divUserCommentPost);
+        });
+
+    });
+            document.body.appendChild(divElement);
+
+
+
+
+
+
+
+
+
+
+
+
