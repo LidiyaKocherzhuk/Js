c688e1c5642efd5d9bc22873979262ab3d07da9c
LidiyaKocherzhuk <lidiyaonufreyiv31@gmail.com>
2021-12-13 18:25:26 +0200

    lesson12

--- lesson12/homework/task2.js
+++ lesson12/homework/task2.js
@@ -1,28 +1,3 @@
-// 1.
-// Отримати відповідь з цього ресурсу відповідь, та вивести в документ об'єкти. Застилізувати, за допомоги css, щоб отримати 5 елементів в рядку.
-// Для кожного елементу свій блок div.post
-// Всі характеристики повинні мати свої блоки всередені div.post
-// https://jsonplaceholder.typicode.com/posts
-//
-
-fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json())
-    .then(responsePosts => {
-            responsePosts.forEach(post => {
-                let divPost = document.createElement('div');
-                divPost.classList.add('posts');
-                let ul = document.createElement('ul');
-                for (const key in post) {
-                    let li = document.createElement('li');
-                    li.innerHTML = `${key} - ${post[key]} `;
-                    ul.appendChild(li);
-                }
-                divPost.appendChild(ul);
-                document.body.appendChild(divPost);
-            });
-        }
-    );
-
-
 
 //     2.
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті.
