e260e188cb85ee3f27f77676398c29f230d2f902
LidiyaKocherzhuk <lidiyaonufreyiv31@gmail.com>
2021-12-13 18:13:50 +0200

    lesson12

--- lesson12/additional/additional12.js
+++ lesson12/additional/additional12.js
@@ -24,22 +24,56 @@ function getComment(placeholder,id, append) {
             })
         })
 }
-            let divElement = document.createElement('div');
+let divElement = document.createElement('div');
+divElement.id = 'users'
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(responseUsers => {
         responseUsers.forEach(user =>{
             let divUserCommentPost = document.createElement('div');
+            divUserCommentPost.classList.add('blocUser')
             let divUser = document.createElement('div');
+            let buttonPosts = document.createElement('button');
+            buttonPosts.innerText = 'open posts'
+            let buttonComments = document.createElement('button');
+            buttonComments.innerText = 'open comments'
+
 
             for (const keyUser in user) {
-                let h3 = document.createElement('h3');
-                h3.innerHTML = `${keyUser} - ${user[keyUser]}`
-                    divUser.appendChild(h3);
-            }
+                    let h3 = document.createElement('h3');
+                    h3.innerHTML = `${keyUser}: - ${user[keyUser]}`
 
-            divUserCommentPost.appendChild(divUser);
+                    getAll(keyUser, user[keyUser], h3)
+
+                    function getAll(key ,obj, tag) {
+                        if (typeof obj === 'object') {
+                            let divIn = document.createElement('div');
+                            divIn.classList.add('marg');
+                            tag.innerHTML = `${key}:`
+                            let inElement = obj;
+                            for (const key in inElement) {
+                                let h5 = document.createElement('h5');
+                                h5.innerHTML = `${key} - ${inElement[key]}`
+                                divIn.appendChild(h5);
+                                console.log(inElement[key])
+                                getAll(key, inElement[key],h5);
+                                tag.appendChild(divIn);
+                            }
+                        }
+                    }
+
+                    divUser.append(h3, buttonPosts, buttonComments);
+                }
+
+            let divPosts = document.createElement('div');
+            divPosts.classList.add('posts')
+            let divComments = document.createElement('div');
+            divPosts.classList.add('comments')
+
+
+
+            divUserCommentPost.append(divUser, divPosts, divComments);
             divElement.appendChild(divUserCommentPost);
         });
 
